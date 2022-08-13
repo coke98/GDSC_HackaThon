cf6c6a8c630d57ed092a2510adefaf43cbdb95ad
soochan <tncksdl05@gmail.com>
2022-08-13 15:57:22 +0900

    initial commit

---
+++ src/main/java/GDSC/HackaThon/HackaThonApplication.java
@@ -0,0 +1,13 @@
+package GDSC.HackaThon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackaThonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackaThonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/GDSC/HackaThon/HackaThonApplicationTests.java
@@ -0,0 +1,13 @@
+package GDSC.HackaThon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackaThonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
